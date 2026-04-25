1ff9e970d007f2d15333be3371bfde7b804d706f
asadk633739-gif <asadk633739@gmail.com>
2026-04-25 23:53:08 +0530

    Update BeastCore.java

--- src/main/java/com/beast/mod/BeastCore.java
+++ src/main/java/com/beast/mod/BeastCore.java
@@ -1,15 +1,15 @@
 package com.beast.mod;
 
-import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BeastCore implements ClientModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("beast-v2");
+public class BeastMod implements ModInitializer {
+    public static final String MOD_ID = "beastmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
-    public void onInitializeClient() {
-        LOGGER.info("BEAST V2: PC Overdrive Mode Initialized!");
-        // Rendering aur Exploration hooks yahan se start honge
+    public void onInitialize() {
+        LOGGER.info("Beast Mod V2 Initialized on Infinix Note 30!");
     }
 }
